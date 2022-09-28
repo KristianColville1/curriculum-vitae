3d057cbcaab373256e4eadcc0d1a12b4ca215599
KristianColville1 <kristiancolville1@gmail.com>
2022-09-28 19:27:42 +0100

    Style projects and add effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
-$('.carousel').carousel({
-  interval: 8000
-})
+$(document).ready(function () {
+  /**
+   * Portfolio projects
+   */
+  var animating = false;
+  var callback = function () {
+    animating = false;
+  };
+
+  $("div.img-container").hover(
+    function () {
+      if (animating) return;
+      animating = true;
+
+      $(this).addClass("active");
+      $(this).next().removeClass("overlay-shine");
+
+      //set small delay
+      setTimeout(callback, 200);
+    },
+    function () {
+      if (animating) return;
+      animating = true;
+
+      $(this).removeClass("active");
+
+      //set small delay
+      setTimeout(callback, 200);
+    }
+  );
+
+});
